694574c740746a3553fa55cedd1f5f00b9b39348
Mohammedaoudi <meddaou2003@gmail.com>
2023-12-04 11:02:47 -0800

    taskssd

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        return(len(sentence), None)
-    return ((len(sentence), sentence[0]))
+    if not sentence:
+        sentence = None
+    if sentence:
+        sen_len = len(sentence)
+    else:
+        sen_len = 0
+    return (sen_len, sentence if not sentence else sentence[:1])
