c56404e91c9baa16e8a31fec14b66f8de7e8bd24
Mohammedaoudi <meddaou2003@gmail.com>
2024-01-19 14:48:01 -0800

    task7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,8 +76,10 @@ class Rectangle(Base):
 
     def display(self):
         """display func"""
+        for j in range(self.__y):
+            print()
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """__str__ methode"""
